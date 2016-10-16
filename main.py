4bf2950c584e5e3f9ba9419d3eb9355b3bf39ac6
m <max@gmail.com>
2016-10-16 15:59:49 +0000

    Fixed main funciton

--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 #this is a project made at hackriddle 2016
 #it is a "smart" toaster using clarifai, simplecv, and twilio
 #by: Jessie Pullaro, Frank Calas, Max Farrel and Kyle Spomer
-
+# -*- coding: utf-8 -*-
+import RPi.GPIO as GPIO
+import time
+import serial
+from twilio.rest import TwilioRestClient
 from clarifai import rest
 from clarifai.rest import ClarifaiApp
+import cv2
+from PIL import Image, ImageEnhance
+ser = serial.Serial('/dev/ttyACM4',9600)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+GPIO.output(2,GPIO.HIGH)
 
+
+#while True :        
+#	val = int(ser.readline())
+#        print(val)
+#        if val == 1 :
+#		break
 
 file = "current_image.png"
 
 
-camera_port = 1
+camera_port = 0
 # Now we can initialize the camera capture object with the cv2.VideoCapture class.
 # All it needs is the index to a camera port.
 camera = cv2.VideoCapture(camera_port)
-#pulls the api keys from keys.py
-app = ClarifaiApp("nnDJHbfgjR6qFYT_zv9RVoMBmR9-vFnvWFMHfm0F", "JqEgorhvy0DGV8PYHwhDg24f3N4yGiRi1HQBS46a")
+app = ClarifaiApp("UHgwtsynZgd19kC1f46FoNulBsdglENG5oF9Dwwc", "VBP5o33oTh6Whpt7nZyg-gAa1GV9ZoKDZktJ4vBe")#pulls the api keys from keys.py
 
 model = app.models.get("toast")
 
-toast_perfect = 0
-
+toast_perfect = 0.3
+toast_done = False
+toast_counter = 0
 # predict with samples
 
 def get_image():
@@ -28,7 +44,7 @@ def get_image():
  return im
 
 
-while (toast_perfect < .75):
+while (toast_done != True or toast_counter == 3):
     camera_capture = get_image()
 
     cv2.imwrite(file, camera_capture)
@@ -41,15 +57,32 @@ while (toast_perfect < .75):
 
     model_json = model.predict_by_filename(file)
 
+    print model_json[u'outputs'][0][u'data'][u'concepts'][0][u'value']
 
-    if (model_json[u'outputs'][0][u'data'][u'concepts'][0][u'id'] == "perfect toast"):
-        toast_perfect = model_json[u'outputs'][0][u'data'][u'concepts'][0][u'value']
+    if (toast_counter < 3 and float(model_json[u'outputs'][0][u'data'][u'concepts'][0][u'value']) > .48):
+        toast_counter = toast_counter + 1
+	print ("toast done is true")
+	print (toast_counter)
+    if (toast_counter >= 3):
+	toast_done = True
+	print ("Toast RIP")
+	break
+    else:
+	print ("toast done is false")
+	continue
+    
+    print (toast_perfect)
+    toast_perfect = model_json[u'outputs'][0][u'data'][u'concepts'][0][u'value']
+    print ("\n")
 
 
+client = TwilioRestClient(account='AC24b3c0ad866586038e4f5ca818ae30ed',token='3bab3d6d5bef27855cfb5bada3c39346')
+client.messages.create(to="+14077583284",from_="+14079019282", body="TOAST")
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.OUT)
+GPIO.output(2,GPIO.LOW)
 
-"""
-has to loop and get pictures
-send to api
-take in the thingy
-when it gets to a threshold of perfect
-"""
+time.sleep(1)
+GPIO.output(2,GPIO.HIGH)
+GPIO.cleanup()
+
